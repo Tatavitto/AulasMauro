38299a86773bb2c057eb590d81132158d8ac1d4f
Luiz Tatavitto <132013197+Tatavitto@users.noreply.github.com>
2025-08-05 21:59:40 -0300

    Delete aula2 directory

--- aula2/exercicios 4.2/exer2.dart
+++
@@ -1,6 +0,0 @@
-import 'aluno.dart';
-
-void main() {
-  Aluno aluno = Aluno("Fulano de Tal", "123456");
-  print(aluno);
-}
--- aula2/exercicios 4.2/exer3.dart
+++
@@ -1,7 +0,0 @@
-import 'aluno.dart';
-
-void main() {
-  Aluno aluno = Aluno("Fulano de Tal", "123456");
-  aluno.lancaNota(6.3);
-  print(aluno);
-}
